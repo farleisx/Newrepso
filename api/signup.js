5cde3669b9a3062e64d67a42c982d78d2cc09dc5
farleisx <laroxmafox@gmail.com>
2025-09-28 19:16:27 +0300

    Update signup.js

--- api/signup.js
+++ api/signup.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
     res.status(201).json({ message: 'User created' })
   } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: 'Internal server error' }) // always returns JSON
+    console.error('Signup error:', err)
+    res.status(500).json({ error: 'Internal server error' })
   }
 }
